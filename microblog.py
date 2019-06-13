bba82764f6898c4cccc603a2a67c040600c4e35b
ant443 <inconspicuous443@gmail.com>
2019-06-13 08:41:25 +0100

    remove database from previous commit and redo migrations

--- microblog.py
+++ microblog.py
@@ -1,4 +1,15 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"db": db, "User": User, "Post": Post}
+
 
 # imports the app variable(the application instance) that is a member of the app package.
 # If you find this confusing, you can rename either the package or the variable to something else.
+
+# flask shell will pre-import app and anything else you wish to pre-import using the function above.
+# Benefits include working with database entities without having to import them.
+
