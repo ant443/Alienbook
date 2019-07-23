d7e3e0ebb705cd4dfa03e22a27a5644a22724646
ant443 <inconspicuous443@gmail.com>
2019-07-23 16:47:26 +0100

    Refactor css and js
    Refactored index.html and signup.html's css using BEM style
    as I think it's easier to read.
    Refactored displayLoginForm function in script.js.
    Made small update to way base.html added hidden class.

--- app/routes.py
+++ app/routes.py
@@ -2,36 +2,48 @@ from flask import render_template, request, flash, redirect, url_for
 from app import app
 from datetime import datetime
 
+
 def valid_user(username, userpass):
     user = {"username": "Miguel"}
     users = {"Miguel": "guess"}
     return username in user and users.get("Miguel", "none") == userpass
 
-@app.route('/', methods=["POST", "GET"])
-@app.route('/index', methods=["POST", "GET"])
+
+@app.route("/", methods=["POST", "GET"])
+@app.route("/index", methods=["POST", "GET"])
 def index():
     user = {"username": "Miguel"}
-    title = "Facebook - log in or sign up"
-    year = year=datetime.now().year
+    title = "Facebook clone - log in or sign up"
+    year = year = datetime.now().year
     # error = None
     if request.method == "POST":
-        if request.form["email_phone"] == "admin" and \
-            request.form["password"] == "secret":
+        if (
+            request.form["email_phone"] == "admin"
+            and request.form["password"] == "secret"
+        ):
             flash("login successful, welcome user")
             return redirect(url_for("index"))
-            # return render template with loged in pannel
+            # (note: fb has completely different page when logged in, but still shows www.facebook.com in the address bar)
         else:
             return redirect(url_for("failedlogin"))
 
     return render_template("index.html", title=title, user=user, year=year)
 
-@app.route("/signup", methods=['GET', 'POST'])
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template("signup.html", title="Sign up to Facebook | Facebook", hidden_menu=True, year=datetime.now().year)
+    return render_template(
+        "signup.html",
+        title="Sign up to Facebook clone | Facebook clone",
+        hidden_menu=True,
+        year=datetime.now().year,
+    )
+
 
 @app.route("/failedlogin", methods=["POST", "GET"])
 def failedlogin():
     return render_template("failed_login.html", title="Log in to Facebook | Facebook")
+
 
 # decorators can be used to register the function that follows them as a callback for a certain event. When a web browser requests either of these two URLs "/" or "index", index will be called and it's return value will be passed back to the browser as a response.
 
@@ -44,13 +56,4 @@ def failedlogin():
 # make sure action attribute in form has the url you want(or url_for). empty string will send data to the form's URL.
 # url_for can be used in place of link passing the name of the view function as it's parameter.
 # make sure name attribute in form matches view functions form.request
-
-# TODO:
-# remake with React and BEM
-# make a page that renders pictures
-# create a logged in component for the right of the nav bar(name, avatar, dropdown menu with items incl. log out)
-# find out about security, how to use secure key ect..
-# signup page does nothing useful, but probably want to continue with the guide for that.
-
-# Current problem:
 
--- app/static/script.js
+++ app/static/script.js
@@ -1,53 +1,10 @@
-// Make Log in button display log in form
-
-// use JS to get button element and hidden_elem, and onclick change hidden_elem to default display.
-// THINK WE HAVE BUTTONS WHERE WE SHOULD HAVE <INPUT TYPE=SUBMIT>
-
-// document.getElementById("display-login").setAttribute("display", "none");
-
 document.getElementById("display-login")
-  .addEventListener("click", displayLogin);
+  .addEventListener("click", displayLoginForm);
 
-function displayLogin() {
-  document.getElementById("display-login").style.display = "none";
-  document.querySelectorAll(".hidden_elem")[0].style.display = "block";
-  // el = document.querySelectorAll(".hidden_elem");
-  // console.log(el[0]);
-  // change display attribute to..
-  // el.classList.remove("")
+function displayLoginForm(e) {
+  this.classList.add("hidden");
+  e.target.nextElementSibling.classList.remove("hidden");
 }
-
-// If you’re hiding text on a FAQ page with the intention of showing it with JS, you should also hide it with JS, otherwise, people using regular CSS enabled browsers but with JS disabled JS will never be able to see the text, leaving you with an unusable FAQ’s page!
-
-// When hiding something with the intention of showing it with JS, you should also hide it with JS, otherwise, people using regular CSS enabled browsers, but with JS disabled, JS will never be able to see the text.
-// Excellent point. Remove it (visually) once the DOM loads.
-
-// front end dev should use a screen reader for a little bit: NVDA, Orca, VoiceOver
-
-// Learn all you can about accessibility, then ask this if still not sure, in reactiflux general:
-// Regarding the "log in to Existing Account" on facebook sign up page that when clicked is replaced by the login form. They use jquery css.hide() and css.show(). How does this affect screen readers?
-
-// ----------------------
-// I always thought positioning elements off the screen to hide them was a hacky method, and display: none was what you where supposed to do but I've just read in a 2012 css-tricks article that display: none is bad for screen readers. I'm duplicating the facebook login on the signup page.It has a button that when clicked disappears and the login form appears in it's place. Fyi, they have used display: none; on the form and use jquery css.hide() and css.show()
-
-// none is good for screen readers when used correctly
-// It says don't consider this element
-// the issue in that setup would be the javascript to hide / show it
-
-// so the screen reader would read the button, and then it could read the form once it becomes display: block ? (after clicking)
-
-// The issue in the setup is the javascript to do that
-
-// what do you mean ?
-
-// The functionality you're describing should not happen for screen readers, there should be no showing/hiding things
-// Because that's not the consideration of a screen reader
-// If you're aiming for accessibility, you should provide a version of your site that works for screen readers and other clients where the common Javascript approaches fail
-// webaim is the best place for reading about accessibility - Archelause
-// --------------------
-
-// /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-
 
 
 
