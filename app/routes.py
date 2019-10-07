e0c8bd66152e04de90cde39a27ffb4bd2072312b
ant443 <inconspicuous443@gmail.com>
2019-10-07 16:40:26 +0100

    remove some duplicate login code in routes

--- app/routes.py
+++ app/routes.py
@@ -9,18 +9,18 @@ from app.models import User
 @app.route("/", methods=["POST", "GET"])
 @app.route("/index", methods=["POST", "GET"])
 def index():
-    user = {"username": "Miguel"}
+    # user = {"username": "Miguel"}
     title = "Alienbook - log in or sign up"
     year = datetime.now().year
     form = LoginForm()
-    if request.method == "POST":
-        print(request, flush=True)
-        if form.validate_on_submit():
-            flash(f"Login requested for user {form.email.data}")
-            return redirect(url_for("index"))
-        else:
-            return redirect(url_for("failed_login"))
-    return render_template("index.html", title=title, user=user, year=year, form=form)
+    # if request.method == "POST":
+    #     print(request, flush=True)
+    #     if form.validate_on_submit():
+    #         flash(f"Login requested for user {form.email.data}")
+    #         return redirect(url_for("index"))
+    #     else:
+    #         return redirect(url_for("login"))
+    return render_template("index.html", title=title, year=year, form=form)
 
 
 @app.route("/signup", methods=["POST", "GET"])
@@ -36,25 +36,25 @@ def signup():
     )
 
 
-@app.route("/failed_login", methods=["POST", "GET"])
-def failed_login():
+@app.route("/login", methods=["POST", "GET"])
+def login():
     if current_user.is_authenticated:
         return redirect(url_for("index"))
     form = LoginForm()
-    if request.method == "POST":
-        print(request, flush=True)
+    # if request.method == "POST":
+    #     print(request, flush=True)
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None or not user.check_password(form.password.data):
             flash("Invalid username or password")
             return render_template(
-                "failed_login.html", title="Log in to Alienbook | Alienbook", form=form
+                "login.html", title="Log in to Alienbook | Alienbook", form=form
             )
         login_user(user)
         flash(f"Login successful for user {form.email.data}")
         return redirect(url_for("index"))
     return render_template(
-        "failed_login.html", title="Log in to Alienbook | Alienbook", form=form
+        "login.html", title="Log in to Alienbook | Alienbook", form=form
     )
 
 
